310e927fa7d487d66fa5855da498077d66c5983a
esraelif <elifesratunca@gmail.com>
2024-05-08 11:50:49 +0200

    "Remove unnecessary arrow function in state declaration and simplify useEffect for keypress event"
    This commit removes an unnecessary arrow function in the state declaration for `wordToGuess` and instead directly assigns the result of `getWord

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { KeyboardEvent, useCallback, useEffect, useState } from "react";
-import words from "./wordList.json";
+import { useCallback, useEffect, useState } from "react";
 import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
 import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState((getWord) => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
-  console.log(wordToGuess);
+  const [wordToGuess, setWordToGuess] = useState(getWord);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   const incorrectLetters = guessedLetters.filter(
     (letter) => !wordToGuess.includes(letter)
   );
@@ -26,6 +24,7 @@ function App() {
   const addGuessedLetter = useCallback(
     (letter: string) => {
       if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
       setGuessedLetters((currentLetters) => [...currentLetters, letter]);
     },
     [guessedLetters, isWinner, isLoser]
@@ -34,28 +33,35 @@ function App() {
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
       const key = e.key;
-      if (!key.match(/'[a-z]$/)) return;
+      if (!key.match(/^[a-z]$/)) return;
+
       e.preventDefault();
       addGuessedLetter(key);
     };
+
     document.addEventListener("keypress", handler);
+
     return () => {
       document.removeEventListener("keypress", handler);
     };
   }, [guessedLetters]);
+
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
       const key = e.key;
       if (key !== "Enter") return;
+
       e.preventDefault();
       setGuessedLetters([]);
       setWordToGuess(getWord());
     };
+
     document.addEventListener("keypress", handler);
+
     return () => {
       document.removeEventListener("keypress", handler);
     };
-  }, [guessedLetters]);
+  }, []);
 
   return (
     <div
@@ -68,14 +74,9 @@ function App() {
         alignItems: "center",
       }}
     >
-      <div
-        style={{
-          fontSize: "2rem",
-          textAlign: "center",
-        }}
-      >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
         {isWinner && "Winner! - Refresh to try again"}
-        {isLoser && "Nice Try! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
       </div>
       <HangmanDrawing numberofGuesses={incorrectLetters.length} />
       <HangmanWord
